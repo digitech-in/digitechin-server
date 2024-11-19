9a4a4d187a6ce168ccc69d4782a2a0dfdb68e0d7
balhyo-younjisang <yjs12180825@gmail.com>
2024-11-19 15:31:31 +0900

    Initialized Repository

---
+++ src/main/java/kr/hs/sdh/digitechin/DigitechinApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.sdh.digitechin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitechinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitechinApplication.class, args);
+    }
+
+}
